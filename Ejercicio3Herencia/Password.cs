fad12a87cbaedc3720b04bb9aa49777f79c42363
davidbarberan9 <davidbarberan9@gmail.com>
2021-03-22 20:53:18 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3Herencia/Password.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio3Herencia
+{
+    public class Password
+    {
+        const int longitudD = 8;
+        int _longitud;
+        String _contraseña;
+
+        public Password() :this(longitudD)
+        {
+
+        }
+        public Password(int longitud)
+        {
+            _contraseña = generarPassword(longitud);
+        }
+
+        public int Longitud { get => _longitud; set => _longitud = value; }
+        public string Contraseña { get => _contraseña;}
+    
+            
+        public Boolean esFuerte()
+        {
+            int mayus = 0, minus = 0, number = 0;
+            char[] pass = this._contraseña.ToCharArray();
+            for (int i = 0; i < pass.Length; i++)
+            {
+                switch (pass[i])
+                {
+                    case char n when (n >= 'A' && n <= 'Z'):mayus++; break;
+                    case char n when (n >= 'a' && n <= 'z'):minus++; break;
+                    case char n when (n >= '0' && n <= '9'):number++; break;
+                }
+                
+            }
+            if (mayus > 2 && minus > 1 && number > 5) 
+                return true;
+            return false;
+        }
+        private String generarPassword(int longitud)
+        {
+            String pas = "";
+            Random r = new Random();
+            for (int i = 0; i < longitud; i++)
+            {
+                int random = r.Next(1, 4);
+                switch (random)
+                {
+                    case 1:pas += (char)r.Next('A','Z'); break;
+                    case 2:pas += (char)r.Next('a', 'z'); break;
+                    case 3:pas += r.Next(0, 9);break;
+                }
+            }
+            return pas;
+        }
+    }
+}
---
+++ Ejercicio3Herencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio3Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el tamaño de el array..");
+            int l = int.Parse(Console.ReadLine());
+            Password[] p = new Password[l];
+            Console.WriteLine("Indique la longitud de las contraseñas");
+            int n= int.Parse(Console.ReadLine());
+            Boolean[] strong = new Boolean[l];
+            for (int i = 0; i < l; i++)
+            {
+                p[i] = new Password(n);
+                strong[i] = p[i].esFuerte();
+                Console.WriteLine("contraseña" + (i + 1) + " " + p[i].Contraseña + "_" + strong[i]);
+            }
+        }
+    }
+}
